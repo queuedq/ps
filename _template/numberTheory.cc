a206c3f21445396b7859e85d30fab3d35e8d7090
Dongkyu Han <queued37@gmail.com>
2020-01-04 02:51:02 +0900

    Update number theory template

--- _template/numberTheory.cc
+++ _template/numberTheory.cc
@@ -8,7 +8,7 @@ lld gcd(lld a, lld b) {
   return a;
 }
 
-pll xgcd(lld a, lld b) {
+pll egcd(lld a, lld b) {
   lld ax = 1, ay = 0, bx = 0, by = 1;
   while (b > 0) {
     lld q = a / b;
@@ -18,6 +18,28 @@ pll xgcd(lld a, lld b) {
   return {ax, ay};
 }
 
-lld modinv(lld a, lld p) {
-  return (xgcd(a, p).first + p) % p;
+lld invmod(lld a, lld m) {
+  return (egcd(a, m).first + m) % m;
+}
+
+lld powmod(lld a, lld e, lld m) {
+  if (e < 0) { a = invmod(a, m); e = -e; }
+  lld b = 1, f = 1;
+  while (f < e) f <<= 1;
+  while (f > 0) {
+    b = b * b % m;
+    if (e & f) b = b * a % m;
+    f >>= 1;
+  }
+  return b;
+}
+
+lld phi(lld n) {
+  lld res = n;
+  for (lld p = 2; p * p <= n; p++) {
+    if (n % p == 0) res = res / p * (p-1);
+    while (n % p == 0) n /= p;
+  }
+  if (n == 1) return res;
+  return res / n * (n-1);
 }
