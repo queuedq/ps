5982ee267e3c1c9df5f882aefbedee556d9aa81d
Dongkyu Han <queued37@gmail.com>
2020-03-06 20:55:46 +0900

    14786 - Ax+Bsin(x)=C ② (재채점)

--- baekjoon/14786/main.cc
+++ baekjoon/14786/main.cc
@@ -15,7 +15,7 @@ int main() {
   int A, B, C; cin >> A >> B >> C;
   double l = 0, r = 200'000;
 
-  while (r - l > 1e-12) {
+  while (r - l > 1e-10) {
     double x = (l + r) / 2;
     if (A*x + B*sin(x) > C) {
       r = x;
@@ -24,7 +24,7 @@ int main() {
     }
   }
 
-  cout << fixed << setprecision(12) << (l + r) / 2 << endl;
+  cout << fixed << setprecision(10) << (l + r) / 2 << endl;
 
   ////////////////////////////////
   return 0;
