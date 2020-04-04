7f31bc666aa308fa5e215a533907a728fbc85164
Dongkyu Han <queued37@gmail.com>
2020-04-04 20:08:01 +0900

    Update segtree template

--- _template/segtree.cc
+++ _template/segtree.cc
@@ -6,7 +6,7 @@ int compress(vector<int> coords, int x) {
 
 // range max query + range addition
 struct SegTree {
-  vector<int> seg, lazy; // lazy: lazy only
+  vector<lld> seg, lazy; // lazy: lazy only
   int size;
 
   SegTree(int sz) {
@@ -26,7 +26,7 @@ struct SegTree {
   }
 
   // non-lazy only
-  void update(int i, int val) {
+  void update(int i, lld val) {
     int n = size + i;
     seg[n] = val;
     while (n > 1) {
@@ -35,8 +35,8 @@ struct SegTree {
     }
   }
 
-  void update_range(int l, int r, int val) { update_range(1, 0, size, l, r, val); }
-  void update_range(int n, int nl, int nr, int l, int r, int val) {
+  void update_range(int l, int r, lld val) { update_range(1, 0, size, l, r, val); }
+  void update_range(int n, int nl, int nr, int l, int r, lld val) {
     propagate(n, nl, nr);
     if (nr <= l || r <= nl) return;
     if (l <= nl && nr <= r) { lazy[n] += val; return propagate(n, nl, nr); }
@@ -46,8 +46,8 @@ struct SegTree {
     seg[n] = max(seg[n*2], seg[n*2+1]);
   }
 
-  int query(int l, int r) { return query(1, 0, size, l, r); }
-  int query(int n, int nl, int nr, int l, int r) {
+  lld query(int l, int r) { return query(1, 0, size, l, r); }
+  lld query(int n, int nl, int nr, int l, int r) {
     propagate(n, nl, nr); // lazy only
     if (nr <= l || r <= nl) return 0;
     if (l <= nl && nr <= r) return seg[n];
