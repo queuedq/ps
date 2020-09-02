0a443800476c2437cbad5958fddc0f869b76beb6
Dongkyu Han <queued37@gmail.com>
2020-09-02 23:19:38 +0900

    BOJ 1764 코드 템플릿 재추가

--- baekjoon/1764/main.cc
+++ baekjoon/1764/main.cc
@@ -1,5 +1,9 @@
 #include <bits/stdc++.h>
+#define endl "\n"
 using namespace std;
+using lld = long long;
+using pii = pair<int, int>;
+using pll = pair<lld, lld>;
 
 ////////////////////////////////////////////////////////////////
 int N, M;
