f34c3cacde4a6dc16c9b3073e3a1f90ced4167b3
Dongkyu Han <queued37@gmail.com>
2020-09-09 21:27:18 +0900

    Update DSU

--- _library/dsu.cc
+++ _library/dsu.cc
@@ -1,25 +1,47 @@
 #include "template.h"
 
-struct DSU {
-  vector<int> parent, size;
+// Disjoint set union
+// 0-based index
+struct disjoint_set {
+  vector<int> p, s;
 
-  DSU(int s) {
-    parent.resize(s); size.resize(s);
-    for (int i = 0; i < s; i++) {
-      parent[i] = i;
-      size[i] = 1;
+  disjoint_set(int sz) {
+    p.resize(sz); s.resize(sz);
+    for (int i = 0; i < sz; i++) {
+      p[i] = i;
+      s[i] = 1;
     }
   }
 
+  // Find the set number containing x
   int find(int x) {
-    if (x == parent[x]) return x;
-    return parent[x] = find(parent[x]);
+    if (x == p[x]) return x;
+    return p[x] = find(p[x]);
   }
 
+  // Merge two sets
+  // Note: "union" is a keyword in C++
   void merge(int x, int y) {
     x = find(x); y = find(y);
-    if (size[x] < size[y]) swap(x, y);
-    parent[y] = x;
-    size[x] += size[y];
+    if (s[x] < s[y]) swap(x, y);
+    p[y] = x;
+    s[x] += s[y];
   }
-} dsu(100'000);
+
+  // Utility functions
+  bool same(int x, int y) { return find(x) == find(y); }
+  int size(int x) { return s[find(x)]; }
+};
+
+// Usage
+int main() {
+  const int size = 25;
+  disjoint_set D(size);
+
+  for (int i=0; i<20; i++) D.merge(i, i+5);
+  assert(D.find(3) == D.find(23));
+  assert(D.same(3, 23));
+  assert(D.size(13) == 5);
+
+  return 0;
+}
