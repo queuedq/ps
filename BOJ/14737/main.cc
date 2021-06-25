8b3cb403d5e80ecc28f5091347682aeea60e34f4
Han Dongkyu <queuedq@gmail.com>
2021-06-26 05:38:12 +0900

    Dev, Please Add This!

--- BOJ/14737/main.cc
+++ BOJ/14737/main.cc
@@ -6,7 +6,7 @@ using pii = pair<int, int>;
 using pll = pair<lld, lld>;
 
 struct scc {
-  int n;
+  int n, scc_cnt;
   vector<vector<int>> g, gt;
   vector<bool> vst;
   vector<int> ord, comp;
@@ -31,29 +31,43 @@ struct scc {
 
   void solve() {
     vst.assign(n, false);
-    for (int i=0; i<n; ++i) {
+    for (int i=0; i<n; i++) {
       if (!vst[i]) dfs1(i);
     }
     reverse(ord.begin(), ord.end());
 
     comp.assign(n, -1);
-    for (int i=0, j=0; i<n; ++i) {
+    scc_cnt = 0;
+    for (int i=0; i<n; i++) {
       int u = ord[i];
-      if (comp[u] == -1) dfs2(u, j++);
+      if (comp[u] == -1) dfs2(u, scc_cnt++);
     }
+  }
+
+  vector<vector<int>> compress() {
+    vector<vector<int>> dag(scc_cnt);
+    set<pii> edges;
+
+    for (int u=0; u<n; u++) {
+      for (auto v: g[u]) {
+        if (comp[u] != comp[v] && edges.count({comp[u], comp[v]}) == 0) {
+          dag[comp[u]].push_back(comp[v]);
+          edges.insert({comp[u], comp[v]});
+        }
+      }
+    }
+    return dag;
   }
 };
 
 struct two_sat: scc {
   // g: 0-based, (even: x, odd: !x)
-  set<pii> clauses;
+  vector<pii> clauses;
   vector<bool> ans;
 
   two_sat(int var): scc(var*2) { }
 
   void add_clause(int a, int b) {
-    if (clauses.count({a, b})) return;
-    clauses.insert({a, b}); clauses.insert({b, a});
     add_edge(a^1, b); add_edge(b^1, a);
   }
 
@@ -71,10 +85,10 @@ struct two_sat: scc {
 
 ////////////////////////////////////////////////////////////////
 const int MH = 55;
-int H, W;
+int H, W, sx, sy, id[MH*MH*2], last_id;
 char level[MH][MH];
 
-inline int node(int i, int j, int k) { return ((i-1)*W + (j-1))*2 + k; }
+inline int node(int i, int j, int k) { return id[((i-1)*W + (j-1))*2 + k]; }
 inline bool blank(int i, int j) { return 1<=i && i<=H && 1<=j && j<=W && level[i][j] != '#'; }
 
 void dfs(int u, vector<vector<int>> &graph, vector<bool> &vst) {
@@ -90,14 +104,20 @@ int main() {
 
   cin >> H >> W;
   for (int i=1; i<=H; i++) {
-    for (int j=1; j<=W; j++) cin >> level[i][j];
+    for (int j=1; j<=W; j++) {
+      cin >> level[i][j];
+      int x = (i-1)*W + (j-1);
+      if (level[i][j] != '#') { id[x*2] = ++last_id; id[x*2+1] = ++last_id; }
+      if (level[i][j] == 'O') { sx = i; sy = j; }
+    }
   }
 
   // SCC
-  scc G(H*W*2);
+  scc G(last_id+1);
 
   for (int i=1; i<=H; i++) {
     for (int j=1; j<=W; j++) {
+      if (level[i][j] == '#') continue;
       int v = node(i, j, 0), h = node(i, j, 1);
       int u = node(i-1, j, 0), d = node(i+1, j, 0);
       int l = node(i, j-1, 1), r = node(i, j+1, 1);
@@ -112,27 +132,32 @@ int main() {
   }
 
   G.solve();
+  int n = G.scc_cnt;
+  auto dag = G.compress();
+
+  // Get reachability
+  vector<vector<bool>> reach(n);
+  for (int i=0; i<n; i++) {
+    reach[i].resize(n);
+    dfs(i, dag, reach[i]);
+  }
 
   // 2-SAT
-  two_sat T(H*W*2);
+  two_sat T(n);
+
+  for (int i=0; i<n; i++) {
+    if (!reach[G.comp[node(sx, sy, 0)]][i] && !reach[G.comp[node(sx, sy, 1)]][i]) {
+      T.add_clause(i*2+1, i*2+1); // unreachable from ball
+    }
+    for (int j=0; j<n; j++) {
+      if (!reach[i][j] && !reach[j][i]) T.add_clause(i*2+1, j*2+1); // at most one node
+    }
+  }
 
   for (int i=1; i<=H; i++) {
     for (int j=1; j<=W; j++) {
-      int cell = level[i][j];
-      if (cell == '#' || cell == '.') continue;
-      T.add_clause(G.comp[node(i, j, 0)]*2, G.comp[node(i, j, 1)]*2);
-
-      for (int k: {0, 1}) {
-        int u = node(i, j, k);
-
-        vector<bool> vf(H*W*2), vb(H*W*2);
-        dfs(u, G.g, vf);
-        if (cell == '*') dfs(u, G.gt, vb);
-
-        for (int v=0; v<H*W*2; v++) {
-          if (!vf[v] && !vb[v]) T.add_clause(G.comp[u]*2+1, G.comp[v]*2+1);
-        }
-      }
+      if (level[i][j] == '#' || level[i][j] == '.') continue;
+      T.add_clause(G.comp[node(i, j, 0)]*2, G.comp[node(i, j, 1)]*2); // ball or star
     }
   }
 
