147bee9d7558ec8493450a7ef019797c8437daab
Han Dongkyu <queuedq@gmail.com>
2022-07-19 11:29:04 +0900

    Update template

--- _template/template.cc
+++ _template/template.cc
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #define endl "\n"
 #define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
 using namespace std;
 using lld = long long;
 using pii = pair<int, int>;
