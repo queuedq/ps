496ccb725b498a57e5f021c4b46ed498c2d058a5
Han Dongkyu <queuedq@gmail.com>
2022-07-23 00:25:42 +0900

    Update lib

--- BOJ/10999/lib.cc
+++ BOJ/10999/lib.cc
@@ -10,41 +10,37 @@ const int ST = 1<<20;
 int N, M, K;
 lld seg[ST*2], lazy[ST*2];
 
-void propagate(int n, int nl, int nr) {
-  seg[n] += lazy[n] * (nr-nl+1);
-  if (n < ST) {
-    lazy[n*2] += lazy[n];
-    lazy[n*2+1] += lazy[n];
-  }
+void push(int n, int nl, int nr) {
+  seg[n*2] += lazy[n] * (nr-nl+1)/2; // sum query
+  seg[n*2+1] += lazy[n] * (nr-nl+1)/2;
+  lazy[n*2] += lazy[n];
+  lazy[n*2+1] += lazy[n];
   lazy[n] = 0;
 }
 
-void update(int n, int nl, int nr, int l, int r, lld v) {
-  propagate(n, nl, nr);
+void add_range(int n, int nl, int nr, int l, int r, lld v) {
   if (nr < l || r < nl) return;
   if (l <= nl && nr <= r) {
+    seg[n] += v * (nr-nl+1); // sum query
     lazy[n] += v;
-    return propagate(n, nl, nr);
+    return;
   }
-
-  int mid = (nl+nr+1) / 2;
-  update(n*2, nl, mid-1, l, r, v);
-  update(n*2+1, mid, nr, l, r, v);
-  seg[n] = seg[n*2] + seg[n*2+1];
+  push(n, nl, nr);
+  int m = (nl+nr)/2;
+  add_range(n*2, nl, m, l, r, v);
+  add_range(n*2+1, m+1, nr, l, r, v);
+  seg[n] = seg[n*2] + seg[n*2+1]; // sum query
 }
+void add_range(int l, int r, lld v) { add_range(1, 0, ST-1, l, r, v); }
 
-lld query(int n, int nl, int nr, int l, int r) {
-  propagate(n, nl, nr);
+lld query_sum(int n, int nl, int nr, int l, int r) {
   if (nr < l || r < nl) return 0;
   if (l <= nl && nr <= r) return seg[n];
-
-  int mid = (nl+nr+1) / 2;
-  return query(n*2, nl, mid-1, l, r) + query(n*2+1, mid, nr, l, r);
+  push(n, nl, nr);
+  int m = (nl+nr)/2;
+  return query_sum(n*2, nl, m, l, r) + query_sum(n*2+1, m+1, nr, l, r); // sum query
 }
-
-// overloading
-void update(int l, int r, lld v) { update(1, 0, ST-1, l, r, v); }
-lld query(int l, int r) { return query(1, 0, ST-1, l, r); }
+lld query_sum(int l, int r) { return query_sum(1, 0, ST-1, l, r); }
 
 int main() {
   ios_base::sync_with_stdio(false);
@@ -54,17 +50,17 @@ int main() {
   cin >> N >> M >> K;
   for (int i=1; i<=N; i++) {
     lld x; cin >> x;
-    update(i, i, x);
+    add_range(i, i, x);
   }
 
   for (int q=0; q<M+K; q++) {
     lld a, b, c, d; cin >> a;
     if (a == 1) {
       cin >> b >> c >> d;
-      update(b, c, d);
+      add_range(b, c, d);
     } else {
       cin >> b >> c;
-      cout << query(b, c) << endl;
+      cout << query_sum(b, c) << endl;
     }
   }
 
