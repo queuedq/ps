55613b80d27be211f866c64f1f40cae6869ef49c
Han Dongkyu <queuedq@gmail.com>
2023-07-17 20:37:17 +0900

    BOJ 피라미드 수열

--- BOJ/13435/main.cc
+++ BOJ/13435/main.cc
@@ -4,7 +4,6 @@ using lld = long long;
 
 int main() {
   lld N, M; cin >> N >> M;
-  if (N == 1 || M == 1) return cout << max(N, M) << endl, 0;
   lld g = gcd(N-1, M-1);
   lld n = (N-1)/g, m = (M-1)/g;
   cout << n*m*g+1 - (n-1)*(m-1)/2 << endl;
